650d4eebac17beb00342c2742ddb6eb42b248ceb
aicylshorts <aicylshorts@gmail.com>
2026-05-30 16:41:18 +0100

    EXPAND: Added many more Exness-style pairs (majors, minors, metals, indices, commodities) for Mulham-style sniper trading

--- config.py
+++ config.py
@@ -4,12 +4,25 @@ load_dotenv()
 
 ASSETS = {
     'OANDA': [
+        # Majors
         'EUR_USD', 'GBP_USD', 'USD_JPY', 'AUD_USD', 'USD_CAD', 'USD_CHF', 'NZD_USD',
-        'EUR_GBP', 'EUR_JPY', 'GBP_JPY', 'AUD_JPY', 'XAU_USD', 'XAG_USD',
-        'NATGAS_USD', 'USOIL', 'UKOIL',
-        'US30_USD', 'NAS100_USD', 'DE30_EUR', 'UK100_GBP'
+        # EUR Crosses
+        'EUR_GBP', 'EUR_JPY', 'EUR_AUD', 'EUR_CAD', 'EUR_NZD', 'EUR_CHF',
+        # GBP Crosses
+        'GBP_JPY', 'GBP_AUD', 'GBP_CAD', 'GBP_NZD', 'GBP_CHF',
+        # AUD / NZD Crosses
+        'AUD_JPY', 'AUD_CAD', 'AUD_NZD', 'AUD_CHF',
+        'NZD_JPY', 'NZD_CAD', 'NZD_CHF',
+        # Other Crosses
+        'CAD_JPY', 'CHF_JPY',
+        # Metals
+        'XAU_USD', 'XAG_USD', 'XPT_USD', 'XPD_USD',
+        # Commodities
+        'NATGAS_USD', 'WTICO_USD', 'BCO_USD',
+        # Indices
+        'US30_USD', 'NAS100_USD', 'SPX500_USD', 'UK100_GBP', 'DE30_EUR', 'FR40_EUR', 'JP225_USD'
     ],
-    'BINANCE': ['BTCUSDT', 'ETHUSDT', 'SOLUSDT']
+    'BINANCE': ['BTCUSD', 'ETHUSD', 'SOLUSD', 'XRPUSD', 'ADAUSD']
 }
 
 TIMEFRAMES = ['15m', '1h', '4h']
